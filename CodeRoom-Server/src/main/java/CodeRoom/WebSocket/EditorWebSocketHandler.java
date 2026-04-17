5fc9381a964680412891b50bf710442098d3cdb9
ashish.r <ashish.r@zohocorp.com>
2026-04-17 22:43:15 +0530

    Created Private session with create, join and leave operations

--- CodeRoom-Server/src/main/java/CodeRoom/WebSocket/EditorWebSocketHandler.java
+++ CodeRoom-Server/src/main/java/CodeRoom/WebSocket/EditorWebSocketHandler.java
@@ -1,14 +1,20 @@
 package CodeRoom.WebSocket;
 
+import CodeRoom.MessageHandler.MessageHandler;
+import CodeRoom.RoomHandler.SessionInfo;
 
 import org.springframework.web.socket.*;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import org.json.JSONObject;
 
 import java.util.*;
 
 public class EditorWebSocketHandler extends TextWebSocketHandler {
 
     private static final Set<WebSocketSession> sessions = new HashSet<>();
+
+    private MessageHandler messageHandler = new MessageHandler();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
@@ -20,12 +26,9 @@ public class EditorWebSocketHandler extends TextWebSocketHandler {
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
 
-        // Broadcast to everyone except sender
-        for (WebSocketSession s : sessions) {
-            if (!s.getId().equals(session.getId()) && s.isOpen()) {
-                s.sendMessage(new TextMessage(payload));
-            }
-        }
+        SessionInfo sessionInfo = new SessionInfo();
+        sessionInfo.session = session;
+        messageHandler.handleMessage(payload, sessionInfo);
     }
 
     @Override
